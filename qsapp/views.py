e12e0697c237c32dd71d99bd56bb87e360298add
Brandon Mills <brandon@brandonmills.me>
2016-12-23 11:08:57 -0600

    Modified templates to display metrics from database.

--- qsapp/views.py
+++ qsapp/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Metric
+from .models import DailyGrade
 
 def metrics_list(request):
-    return render(request, 'qsapp/metrics_list.html', {})
+    metrics = Metric.objects.filter(metric_date__lte=timezone.now()).order_by('metric_date')
+    return render(request, 'qsapp/metrics_list.html', {'metrics': metrics})

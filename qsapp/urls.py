b9255538d5952f693b091fa66db1a0b4941ddfb1
Brandon Mills <brandon@brandonmills.me>
2016-12-23 22:25:55 -0600

    Added view and template for detailed metrics as well as CSS for the site.

--- qsapp/urls.py
+++ qsapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.metrics_list, name='metrics_list'),
+    url(r'^metric/(?P<pk>\d+)/$', views.metric_detail, name='metric_detail'),
 ]
--- qsapp/views.py
+++ qsapp/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.utils import timezone
+
 from .models import Metric
 from .models import DailyGrade
+
+from django.shortcuts import render, get_object_or_404
 
 def metrics_list(request):
     metrics = Metric.objects.filter(metric_date__lte=timezone.now()).order_by('metric_date')
     return render(request, 'qsapp/metrics_list.html', {'metrics': metrics})
+
+def metric_detail(request, pk):
+    metric = get_object_or_404(Metric, pk=pk)
+    return render(request, 'qsapp/metric_detail.html', {'metric': metric})
